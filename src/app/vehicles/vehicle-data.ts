8f741035583af15b955917c71e6d030f12707b4c
DeborahK <deborahk@insteptech.com>
2025-09-05 23:08:36 -0700

    Initial code checkin

---
+++ src/app/vehicles/vehicle-data.ts
@@ -0,0 +1,118 @@
+import { Vehicle } from "./vehicle";
+
+export class VehicleData {
+
+  // Famous pirate ships sample data
+  static vehicles: Vehicle[] = [
+    {
+      cargo_capacity: 100,
+      price: 50,
+      crew: 150,
+      films: ['Treasure Island', 'Black Sails'],
+      manufacturer: "Port Royal Shipyards",
+      model: "Galleon",
+      name: "Queen Anne's Revenge",
+      passengers: 20,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 80,
+      price: 0,
+      crew: 100,
+      films: ['Pirates of the Caribbean'],
+      manufacturer: "London Docks",
+      model: "East Indiaman",
+      name: "Black Pearl",
+      passengers: 10,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 60,
+      price: 0,
+      crew: 80,
+      films: ['Cutthroat Island'],
+      manufacturer: "Spanish Main",
+      model: "Brigantine",
+      name: "Adventure Galley",
+      passengers: 15,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 120,
+      price: 0,
+      crew: 200,
+      films: ['Blackbeard: Terror at Sea'],
+      manufacturer: "Bristol Shipyards",
+      model: "Frigate",
+      name: "Royal Fortune",
+      passengers: 25,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 90,
+      price: 0,
+      crew: 60,
+      films: ['Pirates of the Caribbean: Dead Man\'s Chest'],
+      manufacturer: "Tortuga Shipwrights",
+      model: "Schooner",
+      name: "Flying Dutchman",
+      passengers: 12,
+      vehicle_class: "ghost ship"
+    },
+    {
+      cargo_capacity: 70,
+      price: 0,
+      crew: 50,
+      films: ['Peter Pan'],
+      manufacturer: "Neverland Docks",
+      model: "Barque",
+      name: "Jolly Roger",
+      passengers: 8,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 110,
+      price: 0,
+      crew: 120,
+      films: ['The Sea Hawk'],
+      manufacturer: "Portsmouth Naval Yard",
+      model: "Man-of-war",
+      name: "Revenge",
+      passengers: 18,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 85,
+      price: 0,
+      crew: 90,
+      films: ['Pirates: Blood Brothers'],
+      manufacturer: "Barbary Coast Shipbuilders",
+      model: "Xebec",
+      name: "Whydah Gally",
+      passengers: 14,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 95,
+      price: 0,
+      crew: 70,
+      films: ['Pirate Hunter'],
+      manufacturer: "Jamaica Shipyards",
+      model: "Sloop",
+      name: "Royal James",
+      passengers: 9,
+      vehicle_class: "pirate ship"
+    },
+    {
+      cargo_capacity: 130,
+      price: 0,
+      crew: 160,
+      films: ['Pirates of the Caribbean: At World\'s End'],
+      manufacturer: "Singapore Shipwrights",
+      model: "Junk",
+      name: "Empress",
+      passengers: 30,
+      vehicle_class: "pirate ship"
+    }
+  ]
+}
---
+++ src/app/vehicles/vehicle.ts
@@ -0,0 +1,11 @@
+export interface Vehicle {
+  cargo_capacity: number;
+  crew: number;
+  name: string;
+  model: string;
+  manufacturer: string,
+  price: number,
+  passengers: number;
+  vehicle_class: string;
+  films: string[];
+}
